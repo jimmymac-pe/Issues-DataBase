32503e87b5e61c8cdc4c20e8eb22923caf43f484
Jim McEnteggart <jim.mcenteggart@gmail.com>
2011-06-06 22:16:59 -0400

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IDB001::Application.config.secret_token = '372ce08943dd8a680e523822340c6a1078ec01f2fd58ed31e67008494a3df62c0f78902eab1bb20480c0f6f97903ce3d92399ea6c719004b18027235337c15f1'
